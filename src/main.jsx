bc019e1063cbf07274f0d70cfc7022893a6c8041
Rharhuky <rharhuky@gmail.com>
2025-02-25 22:15:28 -0300

    feat ( main ) : rotas

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import Home from './pages/Home.jsx'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Home from "./pages/Home.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import CriarUsuario from "./pages/CriarUsuario.jsx";
+import Header from "./components/Header.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
   },
+  {
+    path: "/usuarios",
+    element: <CriarUsuario />,
+  },
 ]);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <Header />
     <RouterProvider router={router} />
-  </StrictMode>,
-)
+  </StrictMode>
+);
