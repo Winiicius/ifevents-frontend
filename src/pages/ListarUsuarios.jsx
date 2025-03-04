c0d11c4bb978ecb04f2fbfb8d827eb7cafbda979
Winicius Alexandre <winiciusalexandre@gmail.com>
2025-03-04 00:08:49 -0300

    refact: Mostrar um parágrafo caso não haja usuarios cadastrados

--- src/pages/ListarUsuarios.jsx
+++ src/pages/ListarUsuarios.jsx
@@ -29,7 +29,6 @@ function ListarUsuarios() {
     api
       .get("/usuarios")
       .then((response) => {
-        console.log(response.data);
         const r = response.data;
         setUsuarios(r);
       })
@@ -47,7 +46,7 @@ function ListarUsuarios() {
           </h1>
           <button
             onClick={() => criarUsuario()}
-            className="absolute right-0 px-6 py-2 bg-green-500 mr-2 text-white rounded-lg font-bold hover:bg-blue-600 cursor-pointer"
+            className="absolute right-0 px-6 py-2 bg-green-500 mr-2 text-white rounded-lg font-bold hover:bg-green-700 cursor-pointer"
           >
             Criar usuário
           </button>
@@ -56,48 +55,48 @@ function ListarUsuarios() {
         {/* <div className="mb-5 w-full md:w-3/4 lg:w-3/4 mx-auto p-10 bg-white rounded-lg shadow-md "></div> */}
 
         <div className="mt-2 w-full md:w-3/4 lg:w-3/4 mx-auto p-10 bg-white rounded-lg shadow-md">
-          {/* {usuarios.lenght > 0 ? ( */}
-          <table className="min-w-full bg-gray-200 border border-gray-200">
-            <thead className="">
-              <tr>
-                <Th nome={"Id"} />
-                <Th nome={"Nome"} />
-                <Th nome={"Email"} />
-                <Th nome={"Tipo"} />
-                <Th nome={"Editar"} />
-                <Th nome={"Deletar"} />
-              </tr>
-            </thead>
-            <tbody>
-              {usuarios.map((usuario) => {
-                return (
-                  <tr key={usuario.id}>
-                    <Td atributo={usuario.id} />
-                    <Td atributo={usuario.nome} />
-                    <Td atributo={usuario.email} />
-                    <Td atributo={usuario.tipo} />
-                    <td className="px-4 py-2 border border-black ">
-                      <button
-                        onClick={() => editarUsuario(usuario)}
-                        className="bg-green-500 w-full h-full my-2 text-white rounded-md cursor-pointer"
-                      >
-                        Editar
-                      </button>
-                    </td>
-                    <td className="px-4 py-2 border border-black ">
-                      <button
-                        onClick={() => deletarUsuario(usuario.id)}
-                        className="bg-green-500 w-full h-full my-2 text-white rounded-md cursor-pointer"
-                      >
-                        Deletar
-                      </button>
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-          {/* ) : (<p>Nenhum usuário cadastrado</p>)} */}
+          {usuarios.length > 0 ? (
+            <table className="min-w-full bg-gray-200 border border-gray-200">
+              <thead className="">
+                <tr>
+                  <Th nome={"Id"} />
+                  <Th nome={"Nome"} />
+                  <Th nome={"Email"} />
+                  <Th nome={"Tipo"} />
+                  <Th nome={"Editar"} />
+                  <Th nome={"Deletar"} />
+                </tr>
+              </thead>
+              <tbody>
+                {usuarios.map((usuario) => {
+                  return (
+                    <tr key={usuario.id}>
+                      <Td atributo={usuario.id} />
+                      <Td atributo={usuario.nome} />
+                      <Td atributo={usuario.email} />
+                      <Td atributo={usuario.tipo} />
+                      <td className="px-4 py-2 border border-black ">
+                        <button
+                          onClick={() => editarUsuario(usuario)}
+                          className="bg-green-500 w-full h-full my-2 text-white rounded-md cursor-pointer"
+                        >
+                          Editar
+                        </button>
+                      </td>
+                      <td className="px-4 py-2 border border-black ">
+                        <button
+                          onClick={() => deletarUsuario(usuario.id)}
+                          className="bg-green-500 w-full h-full my-2 text-white rounded-md cursor-pointer"
+                        >
+                          Deletar
+                        </button>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          ) : (<p className="text-center">Nenhum usuário cadastrado</p>)}
         </div>
         <footer></footer>
       </div>
