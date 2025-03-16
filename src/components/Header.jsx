90af68b645e60e5c4b3edd747b6f87d4007d2d6b
Winicius Alexandre <winiciusalexandre@gmail.com>
2025-03-15 23:56:14 -0300

    refact: Deixa o componente Header dinâmico baseado na rota atual

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,26 @@
+import HeaderLogin from "./HeaderLogin";
+import MainHeader from "./MainHeader";
+import { useLocation } from "react-router-dom";
+
 function Header() {
+
+  const location = useLocation(); // Hook para obter a rota atual
+
+  const conteudoHeader = () => {
+    switch (location.pathname) {
+      case '/login':
+        return <HeaderLogin title="Realizar Login" />
+      case '/cadastro':
+        return <HeaderLogin title="Realizar Cadastro" />
+      case '/recuperar-senha':
+        return <HeaderLogin title="Recuperar Senha" />
+      default:
+        return <MainHeader />
+    }
+  };
+
   return (
-    <nav className="w-screen h-20  bg-green-500 flex justify-between items-center text-white">
-      <h3 className="font-bold ml-10 text-4xl">
-        <a href="/">IFEvents</a>
-      </h3>
-      <div className="font-bold mr-6">
-        <a href="" className="text-right p-2">
-          sobre
-        </a>
-        <a href="" className="p-2">
-          sair
-        </a>
-      </div>
-    </nav>
-  );
+    conteudoHeader()
+  )
 }
 export default Header;
---
+++ src/components/HeaderLogin.jsx
@@ -0,0 +1,7 @@
+function HeaderLogin({ title }) {
+    return <nav className="w-screen h-20 bg-green-500 flex justify-center items-center text-white">
+        <h1 className="font-bold text-5xl">{title}</h1>
+    </nav>
+}
+
+export default HeaderLogin;
