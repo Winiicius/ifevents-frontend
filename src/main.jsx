b66c3ae9504db06dbf6d1e5e845246257b6f3fd1
Winicius Alexandre <winiciusalexandre@gmail.com>
2025-03-15 23:59:48 -0300

    refact: Atualiza main

--- src/main.jsx
+++ src/main.jsx
@@ -1,35 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import ListarUsuarios from "./pages/ListarUsuarios.jsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import CriarUsuario from "./pages/CriarUsuario.jsx";
-import Header from "./components/Header.jsx";
-import { Evento } from "./components/evento/Evento.jsx";
-import CriarEvento from "./pages/evento/CriarEvento.jsx";
+import App from "./App.jsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <ListarUsuarios />,
-  },
-  {
-    path: "/usuarios",
-    element: <CriarUsuario />,
-  },
-  {
-    path: "/eventos",
-    element: <Evento />,
-  },
-  {
-    path: "/criar-evento",
-    element: <CriarEvento />
-  }
-]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Header />
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>
 );
