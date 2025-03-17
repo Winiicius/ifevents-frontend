c6b512c2ff359b8faaefacfbe1a6bbac79143bab
Winicius Alexandre <winiciusalexandre@gmail.com>
2025-03-17 20:02:51 -0300

    feat: adiciona controle de acesso as rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/login/Login";
 // import Cadastro from "./pages/Cadastro";
 // import RecuperarSenha from "./pages/RecuperarSenha";
@@ -7,6 +7,9 @@ import { Evento } from "./components/evento/Evento";
 import CriarEvento from "./pages/evento/CriarEvento";
 import ListarUsuarios from "./pages/ListarUsuarios";
 import Layout from "./layouts/Layout";
+import { ProtectedLayout } from "./layouts/ProtectedLayout";
+import AcessoNegado from "./pages/AcessoNegado";
+import PaginaNaoEncontrada from "./pages/PaginaNaoEncontrada"
 
 
 function App() {
@@ -17,10 +20,18 @@ function App() {
                     <Route path="login" element={<Login />} />
                     {/* <Route path="cadastro" element={<Cadastro />} /> */}
                     {/* <Route path="recuperar-senha" element={<RecuperarSenha />} /> */}
-                    <Route path="criar-usuario" element={<CriarUsuario />} />
-                    <Route path="usuarios" element={<ListarUsuarios />} />
-                    <Route path="eventos" element={<Evento />} />
+                    <Route path="acesso-negado" element={<AcessoNegado />} />
+
+                    <Route element={<ProtectedLayout roles={["COORDENADOR"]} />}>
+                        <Route path="eventos" element={<Evento />} />
+                        <Route path="usuarios" element={<ListarUsuarios />} />
+                    </Route>
+                    <Route element={<ProtectedLayout roles={["COORDENADOR", "PROFESSOR", "ALUNO"]} />}>
+                        <Route path="criar-usuario" element={<CriarUsuario />} />
+                    </Route>
                     <Route path="criar-evento" element={<CriarEvento />} />
+                    <Route path="*" element={<Navigate to="/pagina-nao-encontrada" replace />} />
+                    <Route path="pagina-nao-encontrada" element={<PaginaNaoEncontrada />} />
                 </Route>
             </Routes>
         </BrowserRouter>
