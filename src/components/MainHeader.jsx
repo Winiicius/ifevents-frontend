47d8b943554885183f5a272a66017602be5c41ee
Winicius Alexandre <winiciusalexandre@gmail.com>
2025-03-18 00:50:29 -0300

    feat: adiciona logo no Header e na aba do navegador

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -1,7 +1,7 @@
 function MainHeader() {
     return (<nav className="w-screen h-20 bg-green-500 flex justify-between items-center text-white">
-        <h3 className="font-bold ml-10 text-4xl">
-            <a href="/">IFEvents</a>
+        <h3 className="font-bold ml-5 text-4xl">
+            <a href="/"><img src="src\assets\logo-nav.svg" alt="Logo IFEvents" className="w-50" /></a>
         </h3>
         <div className="font-bold mr-6">
             <a href="" className="text-right p-2">
